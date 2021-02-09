ed2edd025674b020ede1c78f97527f7e43235abe
Raj <raj.rajeev2244@gmail.com>
2021-02-09 11:54:00 +0530

    added login method() with 2 paramaters and return type boolean

--- Customer.java
+++ Customer.java
@@ -1,5 +1,16 @@
 class Customer 
 {
+	public boolean login(String username,String password)
+	{
+		if(username.equals("admin") && password.equals("admin")
+		{
+			return true;
+		}
+		else
+		{
+			return false
+		}
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome To Customer Class");
